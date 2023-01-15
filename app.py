4ae6989dbd0060daeb68965f50e26232ff294163
deepak170601 <deepakreddy1431@gmail.com>
2023-01-15 23:43:52 +0530

    try 7

--- app.py
+++ app.py
@@ -88,7 +88,3 @@ def delete(id):
     flash('"{}" was successfully deleted!'.format(post['title']))
     return redirect(url_for('index'))
 
-if __name__ == "__main__" :
-    print("hi")
-    app.run(host="0.0.0.0")
-
